46a70ec70a2636a11a6513477dbcd6191e6288ac
lixhium <144508189+lixhium@users.noreply.github.com>
2023-12-21 15:08:59 +0800

    Update remap.lua

--- nvim/lua/adji/remap.lua
+++ nvim/lua/adji/remap.lua
@@ -1,10 +1,1 @@
-vim.g.mapleader = ","
-vim.keymap.set("i", "<leader>.", "<Esc>")
-vim.keymap.set("n", "<leader>e", vim.cmd.q)
-vim.keymap.set("n", "<leader>u", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>t", vim.cmd.terminal)
 
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
