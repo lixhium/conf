920430693774be826b5bb7daa98db5c5677ce8d6
lixhium <notkoishi@gmail.com>
2023-12-31 05:14:07 +0800

    lol

--- nvim/lua/adji/packer.lua
+++ nvim/lua/adji/packer.lua
@@ -16,8 +16,4 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
-
-    use 'folke/tokyonight.nvim'
-
-    use "lukas-reineke/indent-blankline.nvim"
 end)
